5fc8a7bb3cfe465e51bdae856eef004a724a4337
Julia <julialynn396@gmail.com>
2021-07-18 23:29:26 -0500

    flask api with a haunt endpoint

--- api.py
+++ api.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_pymongo import PyMongo
 
+# App
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+# Database
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
-api.add_resource(HelloWorld, '/')
+
+# Haunt
+haunt = {}
+
+class Haunt(Resource):
+    def get(self, haunt_id):
+        return haunt_id
+        # return "test get " + str(haunt_id)
+        # return {haunt_id: haunt[haunt_id]}
+
+api.add_resource(Haunt, '/haunt/<int:haunt_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
